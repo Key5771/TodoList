7926529c3abb3587c62e3933e7924a4ee18b8bd9
key5771 <ksj57715@hanmail.net>
2020-10-16 20:59:37 +0900

    Add CollectionView

---
+++ TodoList/TodoList/View/ViewController.swift
@@ -0,0 +1,38 @@
+//
+//  ViewController.swift
+//  TodoList
+//
+//  Created by 김기현 on 2020/10/16.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+    @IBOutlet weak var collectionView: UICollectionView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        self.collectionView.delegate = self
+        self.collectionView.dataSource = self
+    }
+
+
+}
+
+extension ViewController: UICollectionViewDelegate {
+    
+}
+
+extension ViewController: UICollectionViewDataSource {
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return 1
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        return UICollectionViewCell()
+    }
+    
+    
+}
+
