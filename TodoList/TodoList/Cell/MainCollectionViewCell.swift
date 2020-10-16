9c281bc99349bf4cc42c60caba2a1d9ecad59633
key5771 <ksj57715@hanmail.net>
2020-10-16 20:59:48 +0900

    Make CollectionViewCell

---
+++ TodoList/TodoList/Cell/MainCollectionViewCell.swift
@@ -0,0 +1,19 @@
+//
+//  MainCollectionViewCell.swift
+//  TodoList
+//
+//  Created by 김기현 on 2020/10/16.
+//
+
+import UIKit
+
+class MainCollectionViewCell: UICollectionViewCell {
+    @IBOutlet weak var titleLabel: UILabel!
+    @IBOutlet weak var taskLabel: UILabel!
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+}
