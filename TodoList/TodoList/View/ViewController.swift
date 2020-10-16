4ad8b7ab483ea51f6667b9d012b371e1cadc1a59
key5771 <ksj57715@hanmail.net>
2020-10-16 21:32:49 +0900

    Add dummy data in collectionView cell

--- TodoList/TodoList/View/ViewController.swift
+++ TodoList/TodoList/View/ViewController.swift
@@ -15,24 +15,40 @@ class ViewController: UIViewController {
         
         self.collectionView.delegate = self
         self.collectionView.dataSource = self
+        self.collectionView.register(UINib(nibName: "MainCollectionViewCell", bundle: nil), forCellWithReuseIdentifier: "reuseCell")
     }
 
 
 }
 
 extension ViewController: UICollectionViewDelegate {
-    
+    func collectionView(_ collectionView: UICollectionView, didSelectItemAt indexPath: IndexPath) {
+        self.collectionView.deselectItem(at: indexPath, animated: true)
+    }
 }
 
 extension ViewController: UICollectionViewDataSource {
     func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
-        return 1
+        return 3
     }
     
     func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
-        return UICollectionViewCell()
+        guard let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "reuseCell", for: indexPath) as? MainCollectionViewCell else {
+            return UICollectionViewCell()
+        }
+        
+        cell.titleLabel.text = "Hello World"
+        cell.taskLabel.text = "Hello Swift"
+        
+        return cell
     }
-    
-    
+}
+
+extension ViewController: UICollectionViewDelegateFlowLayout {
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
+        let width = collectionView.frame.width
+        let height = width * 9 / 16 + 55.5
+        return CGSize(width: width, height: height)
+    }
 }
 
