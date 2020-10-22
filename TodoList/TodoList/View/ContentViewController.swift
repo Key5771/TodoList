877d15119263f7db1e805cbf3d0c6366e02dfa87
key5771 <ksj57715@hanmail.net>
2020-10-22 17:09:55 +0900

    Create ContentViewController Create Todo Model

---
+++ TodoList/TodoList/View/ContentViewController.swift
@@ -0,0 +1,29 @@
+//
+//  ContentViewController.swift
+//  TodoList
+//
+//  Created by 김기현 on 2020/10/22.
+//
+
+import UIKit
+
+class ContentViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
