f9c241e3ec9fa36cfca9ec990f89bc21a6af5b6f
key5771 <ksj57715@hanmail.net>
2020-10-23 13:29:01 +0900

    Content TableView Setting

--- TodoList/TodoList/View/ContentViewController.swift
+++ TodoList/TodoList/View/ContentViewController.swift
@@ -10,6 +10,8 @@ import UIKit
 class ContentViewController: UIViewController {
     @IBOutlet weak var categoryLabel: UILabel!
     @IBOutlet weak var taskLabel: UILabel!
+    @IBOutlet weak var tableView: UITableView!
+    
     @IBOutlet var swipeGesture: UISwipeGestureRecognizer!
     
     var categoryName: String?
@@ -17,8 +19,10 @@ class ContentViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        // Do any additional setup after loading the view.
         categoryLabel.text = categoryName
+        self.tableView.delegate = self
+        self.tableView.dataSource = self
+        self.tableView.register(UINib(nibName: "ContentTableViewCell", bundle: nil), forCellReuseIdentifier: "contentCell")
     }
 
     @IBAction func leftSwipe(_ sender: Any) {
@@ -27,4 +31,28 @@ class ContentViewController: UIViewController {
         }
     }
 
+}
+
+extension ContentViewController: UITableViewDelegate {
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        self.tableView.deselectRow(at: indexPath, animated: true)
+    }
+}
+
+extension ContentViewController: UITableViewDataSource {
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 1
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        guard let cell = tableView.dequeueReusableCell(withIdentifier: "contentCell", for: indexPath) as? ContentTableViewCell else {
+            return UITableViewCell()
+        }
+        
+        cell.todoLabel.text = "TEST"
+        
+        return cell
+    }
+    
+    
 }
