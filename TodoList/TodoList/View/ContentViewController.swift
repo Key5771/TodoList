cd6430e1deb15549b08f00d21fdf74ba2a7c4de9
key5771 <ksj57715@hanmail.net>
2020-10-27 13:41:59 +0900

    Delete AddTodoViewController

--- TodoList/TodoList/View/ContentViewController.swift
+++ TodoList/TodoList/View/ContentViewController.swift
@@ -11,6 +11,10 @@ import CoreData
 class ContentViewController: UIViewController {
     @IBOutlet weak var tableView: UITableView!
     
+    @IBOutlet weak var categoryLabel: UILabel!
+    @IBOutlet weak var taskLabel: UILabel!
+    @IBOutlet weak var createButton: UIButton!
+    
     @IBOutlet var swipeGesture: UISwipeGestureRecognizer!
     
     var categoryName: String?
@@ -23,9 +27,14 @@ class ContentViewController: UIViewController {
         self.tableView.delegate = self
         self.tableView.dataSource = self
         
-        self.tableView.register(UINib(nibName: "ContentTableViewHeader", bundle: nil), forHeaderFooterViewReuseIdentifier: "contentHeader")
-        self.tableView.register(UINib(nibName: "ContentTableViewFooter", bundle: nil), forHeaderFooterViewReuseIdentifier: "contentFooter")
         self.tableView.register(UINib(nibName: "ContentTableViewCell", bundle: nil), forCellReuseIdentifier: "contentCell")
+        
+        categoryLabel.text = categoryName
+        if let count = taskCount {
+            taskLabel.text = "\(count) task"
+        } else {
+            taskLabel.text = "0 task"
+        }
     }
     
     override func viewWillAppear(_ animated: Bool) {
@@ -41,7 +50,11 @@ class ContentViewController: UIViewController {
             self.navigationController?.popViewController(animated: true)
         }
     }
-
+    
+    @IBAction func createTodo(_ sender: Any) {
+        print("click")
+    }
+    
 }
 
 // MARK: - UITableViewDelegate
@@ -53,63 +66,14 @@ extension ContentViewController: UITableViewDelegate {
 
 // MARK: - UITableViewDataSource
 extension ContentViewController: UITableViewDataSource {
-    // MARK: - TableView Header
-    func tableView(_ tableView: UITableView, viewForHeaderInSection section: Int) -> UIView? {
-        guard let header = tableView.dequeueReusableHeaderFooterView(withIdentifier: "contentHeader") as? ContentTableViewHeader else {
-            return UIView()
-        }
-        
-        header.categoryLabel.text = categoryName
-        if let count = taskCount {
-            header.taskLabel.text = "\(count) task"
-        }
-        
-        return header
-    }
-    
-    func tableView(_ tableView: UITableView, heightForHeaderInSection section: Int) -> CGFloat {
-        let width = self.view.frame.width
-        let height = self.view.frame.height
-        
-        if width < height {
-            return height / 6
-        } else {
-            return width / 5
-        }
-    }
-    
-    // MARK: - TableView Footer
-    @objc func createTodo() {
-        let vc = AddTodoViewController(nibName: "AddTodoViewController", bundle: nil)
-        vc.category = self.categoryName
-        self.navigationController?.pushViewController(vc, animated: true)
-    }
-    
-    func tableView(_ tableView: UITableView, viewForFooterInSection section: Int) -> UIView? {
-        guard let footer = tableView.dequeueReusableHeaderFooterView(withIdentifier: "contentFooter") as? ContentTableViewFooter else {
-            return UIView()
-        }
-        
-//        footer.createTodoButton.addTarget(self, action: #selector(createTodo), for: .touchUpInside)
-        
-        return footer
-    }
-    
-    func tableView(_ tableView: UITableView, heightForFooterInSection section: Int) -> CGFloat {
-        return 50
-    }
-    
-    // MARK: - TableView Cell
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-//        guard let sections = self.controller?.sections else {
-//            fatalError("No sections in fetchedResultsController at ContentViewController")
-//        }
-//
-//        let sectionInfo = sections[section]
-//        self.taskCount = sectionInfo.numberOfObjects
-//        return sectionInfo.numberOfObjects
-        
-        return 3
+        guard let sections = self.controller?.sections else {
+            fatalError("No sections in fetchedResultsController at ContentViewController")
+        }
+
+        let sectionInfo = sections[section]
+        self.taskCount = sectionInfo.numberOfObjects
+        return sectionInfo.numberOfObjects
     }
     
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
