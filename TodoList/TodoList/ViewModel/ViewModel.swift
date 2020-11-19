4cf917d3f88a0991612135b5a419b06de605cff2
key5771 <ksj57715@hanmail.net>
2020-11-19 21:19:17 +0900

    제네릭으로 변경한 함수 다시 String(Optional)으로 바꿈

--- TodoList/TodoList/ViewModel/ViewModel.swift
+++ TodoList/TodoList/ViewModel/ViewModel.swift
@@ -10,7 +10,7 @@ import UIKit
 import CoreData
 
 protocol ViewModelDelegate {
-    func saveData<T: Codable>(entityName: T, categoryName: T, todoName: T?, date: Date)
+    func saveData(entityName: String, categoryName: String, todoName: String?, date: Date)
 }
 
 class ViewModel {
@@ -19,13 +19,12 @@ class ViewModel {
 }
 
 extension ViewModel: ViewModelDelegate {
-    func saveData<T>(entityName: T, categoryName: T, todoName: T? = nil, date: Date) where T : Decodable, T : Encodable {
+    func saveData(entityName: String, categoryName: String, todoName: String? = nil, date: Date) {
         guard let appDelegate = appDelegate else { return }
         
         let managedContext = appDelegate.persistentContainer.viewContext
         managedContext.automaticallyMergesChangesFromParent = true
         
-        guard let entityName = entityName as? String else { return }
         guard let entity = NSEntityDescription.entity(forEntityName: entityName, in: managedContext) else { return }
         let todo = NSManagedObject(entity: entity, insertInto: managedContext)
         
