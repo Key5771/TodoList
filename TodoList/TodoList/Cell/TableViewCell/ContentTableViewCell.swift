f6756b8e9fa7b17312323cbfac62e7d4c23d7c08
key5771 <ksj57715@hanmail.net>
2023-06-25 15:43:51 +0900

    #5 ContentViewController xib 제거 및 SnapKit 적용

--- TodoList/TodoList/Cell/TableViewCell/ContentTableViewCell.swift
+++ TodoList/TodoList/Cell/TableViewCell/ContentTableViewCell.swift
@@ -6,20 +6,46 @@
 //
 
 import UIKit
+import SnapKit
 
 class ContentTableViewCell: UITableViewCell {
-    @IBOutlet weak var todoLabel: UILabel!
-    @IBOutlet weak var todoLabelView: UIView!
+    static let identifier = "ContentTableViewCell"
     
-    override func awakeFromNib() {
-        super.awakeFromNib()
-        // Initialization code
-    }
-
-    override func setSelected(_ selected: Bool, animated: Bool) {
-        super.setSelected(selected, animated: animated)
-
-        // Configure the view for the selected state
+    private let todoLabel = UILabel()
+    private let leftView: UIView = {
+        let view = UIView()
+        view.backgroundColor = .black
+        return view
+    }()
+    
+    override init(style: UITableViewCell.CellStyle, reuseIdentifier: String?) {
+        super.init(style: style, reuseIdentifier: reuseIdentifier)
+        setup()
     }
     
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    private func setup() {
+        contentView.addSubview(leftView)
+        leftView.snp.makeConstraints { make in
+            make.centerY.equalToSuperview()
+            make.left.equalToSuperview().offset(16)
+            make.width.equalTo(24)
+            make.height.equalTo(2)
+        }
+        
+        contentView.addSubview(todoLabel)
+        todoLabel.snp.makeConstraints { make in
+            make.centerY.equalToSuperview()
+            make.left.equalTo(leftView.snp.right).offset(16)
+            make.right.equalToSuperview().inset(16)
+        }
+    }
+    
+    func configure(with item: Todo) {
+        todoLabel.font = UIFont.boldSystemFont(ofSize: 14)
+        todoLabel.text = item.todoName
+    }
 }
